9ca3a06d44f11c1f7dfd0ec050813d5333d4992b
sickk <jaakmyr@gmail.com>
2024-08-08 00:58:02 -0700

    connects

--- config/databse.js
+++ config/databse.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.DB_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(process.env.DB_STRING)
 
         console.log("Mongoose Connected... HONK!")
     } catch (e){
--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session)
+const MongoStore = require("connect-mongo")
 const methodOverride = require("method-override");
 const flash = require('express-flash')
 const logger = require("morgan");
-const connectDB = require('./config/database')
+const connectDB = require('./config/databse.js')
+let mainRoutes = require('./routers/mainRoutes')
 
 require("dotenv").config({path: "./config/.env"})
 
 app.set('view engine', 'ejs')
-app.use(app.static('public'))
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -26,15 +27,17 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave:false,
     saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection })
+    store: new MongoStore({ mongoUrl: process.env.DB_STRING }),
 }))
 
 //passport config
-require('./config/passport')(passport)
-app.use(passport.initialize())
-app.use(passport.session())
+// require('./config/passport')(passport)
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 app.use(flash())
+
+app.use("/", mainRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("server is :runningemoji:")
